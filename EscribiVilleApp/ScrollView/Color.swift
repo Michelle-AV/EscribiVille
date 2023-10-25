7b9373b75d6070b2611cfaed60c29b54951a10ae
clxsrdev <99055585+clxsrdev@users.noreply.github.com>
2023-10-25 13:39:57 -0600

    Se añadió el color Gris, Blanco y Café

--- EscribiVilleApp/ScrollView/Color.swift
+++ EscribiVilleApp/ScrollView/Color.swift
@@ -10,4 +10,9 @@ extension Color {
     
     public static let Mar: Color = Color(UIColor(red: 0.11, green: 0.40, blue: 0.65,alpha: 1.00))
     
+    public static let Gris: Color = Color(UIColor(red: 0.18, green: 0.18, blue: 0.18, alpha: 1.00))
+    
+    public static let Blanco: Color = Color(UIColor(red: 0.99, green: 0.96, blue: 0.93, alpha: 1.00))
+    
+    public static let Cafe: Color = Color(UIColor(red: 0.60, green: 0.51, blue: 0.47, alpha: 1.00))
 }
