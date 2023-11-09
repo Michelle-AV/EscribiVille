6b480e10f18d5396404792bd80978426c27cc27e
clxsrdev <99055585+clxsrdev@users.noreply.github.com>
2023-11-09 15:31:08 -0600

    Se terminó el nivel 10

--- EscribiVilleApp/ScrollView/LevelsView/Level10View.swift
+++ EscribiVilleApp/ScrollView/LevelsView/Level10View.swift
@@ -27,6 +27,15 @@ struct Level10View: View {
     @State var input6: String = ""
     @State var display6: String = ""
     
+    @State var input7: String = ""
+    @State var display7: String = ""
+    
+    @State var input8: String = ""
+    @State var display8: String = ""
+    
+    @State var input9: String = ""
+    @State var display9: String = ""
+    
     @State var nivel1: Bool = true
     @State var nivel2: Bool = false
     @State var nivel3: Bool = false
@@ -72,17 +81,18 @@ struct Level10View: View {
                 .frame(width: UISW * 0.8)
                 .opacity(nivel2 ? 1 : 0)
             
-            Image("act4-3")
+            Image("act4-s")
+                .resizable()
+                .scaledToFit()
+                .frame(width: UISW * 0.355)
+                .opacity(nivel3 ? 1 : 0)
+            
+            Image("cuadros")
                 .resizable()
                 .scaledToFit()
                 .frame(width: UISW * 0.8)
-                .padding(.top, 90)
+                .padding(.top, UISH * 0.47)
                 .opacity(nivel3 ? 1 : 0)
-            
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(Color.red, lineWidth: 2)
-                .frame(width: UISW * 0.2, height: UISH * 0.18)
-                .position(x: UISW * 0.804, y: UISH * 0.735)
             
             TextField("", text: $input1)
                 .frame(width: 230, height: 20)
@@ -243,6 +253,86 @@ struct Level10View: View {
                 .background(Color.clear)
                 .position(x: UISW * 0.804, y: UISH * 0.755)
                 .opacity(nivel2 ? 1 : 0)
+            
+            TextField("", text: $input7)
+                .frame(width: 230, height: 20)
+                .font(.largeTitle)
+                .padding(.leading, 20)
+                .position(x: UISW * 0.196, y: UISH * 0.735)
+                .foregroundColor(Color.GrisClaro)
+                .onTapGesture {
+                    display7 = ""
+                    input7 = ""
+                }
+                .onChange(of: input7) { newValue in
+                    if newValue.count >= 3 {
+                        input7 = String(newValue.prefix(3))
+                        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                    }
+                    display7 = input7.uppercased()
+                    checkSombrilla()
+                }.opacity(nivel3 ? 1 : 0)
+            
+            Text("\(display7)")
+                .font(.custom("futura", fixedSize: 70))
+                .bold()
+                .foregroundStyle(.black)
+                .position(x: UISW * 0.196, y: UISH * 0.735)
+                .opacity(nivel3 ? 1 : 0)
+            
+            TextField("", text: $input8)
+                .font(.largeTitle)
+                .padding(.leading, 20)
+                .frame(width: 230, height: UISH)
+                .position(x: UISW * 0.496, y: UISH * 0.735)
+                .foregroundColor(Color.GrisClaro)
+                .onTapGesture {
+                    display8 = ""
+                    input8 = ""
+                }
+                .onChange(of: input8) { newValue in
+                    if newValue.count >= 3 {
+                        input8 = String(newValue.prefix(3))
+                        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                    }
+                    display8 = input8.uppercased()
+                    checkSombrilla()
+                }.opacity(nivel3 ? 1 : 0)
+            
+            Text("\(display8)")
+                .font(.custom("futura", fixedSize: 70))
+                .bold()
+                .foregroundStyle(.black)
+                .background(Color.clear)
+                .position(x: UISW * 0.496, y: UISH * 0.735)
+                .opacity(nivel3 ? 1 : 0)
+            
+            TextField("", text: $input9)
+                .font(.largeTitle)
+                .padding(.leading, 20)
+                .frame(width: 230, height: UISH)
+                .position(x: UISW * 0.804, y: UISH * 0.735)
+                .foregroundColor(Color.GrisClaro)
+                .onTapGesture {
+                    display9 = ""
+                    input9 = ""
+                }
+                .onChange(of: input9) { newValue in
+                    if newValue.count >= 3 {
+                        input9 = String(newValue.prefix(3))
+                        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                    }
+                    display9 = input9.uppercased()
+                    checkSombrilla()
+                }.opacity(nivel3 ? 1 : 0)
+            
+            Text("\(display9)")
+                .font(.custom("futura", fixedSize: 70))
+                .bold()
+                .foregroundStyle(.black)
+                .background(Color.clear)
+                .position(x: UISW * 0.804, y: UISH * 0.735)
+                .opacity(nivel3 ? 1 : 0)
 
             Image("btnSalir")
                 .resizable()
@@ -252,6 +342,27 @@ struct Level10View: View {
                 .onTapGesture {
                     withAnimation(.spring(duration: 0.2)){
                         level10.toggle()
+                        nivel1 = true
+                        nivel2 = false
+                        nivel3 = false
+                        input1 = ""
+                        display1 = ""
+                        input2 = ""
+                        display2 = ""
+                        input3 = ""
+                        display3 = ""
+                        input4 = ""
+                        display4 = ""
+                        input5 = ""
+                        display5 = ""
+                        input6 = ""
+                        display6 = ""
+                        input7 = ""
+                        display7 = ""
+                        input8 = ""
+                        display8 = ""
+                        input9 = ""
+                        display9 = ""
                     }
                 }
             
@@ -263,6 +374,10 @@ struct Level10View: View {
                 .onTapGesture {
                     isMute.toggle()
                 }
+            
+            CongratsView(isCorrect: $isCorrect)
+                .offset(y: isCorrect ? 0 : UIScreen.main.bounds.height + 500)
+            
         }.ignoresSafeArea()
     }
     
@@ -283,6 +398,19 @@ struct Level10View: View {
             }
         }
     }
+    
+    func checkSombrilla(){
+        if(display7 == "SOM" && display8 == "BRI" && display9 == "LLA"){
+            withAnimation(.spring(duration: 0.5)) {
+                nivel3 = true
+            }
+            withAnimation(.spring(duration: 0.3)) {
+                isCorrect.toggle()
+                SoundManager.instance.playSound()
+            }
+        }
+    }
+
     
 }
 
